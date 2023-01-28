6cbb8475197d9a23c5b16dbb25e803db419897bb
Ewerthon-izi <ewer.izi@gmail.com>
2023-01-28 00:53:01 -0300

    add base class of ships, and enum of board

---
+++ battleship/BattleShip/BattleShip/Board/LocaleType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.Board
+{
+    public enum LocaleType
+    {
+        [Description("o")]
+        Empty,
+
+        [Description("B")]
+        Battleship,
+
+        [Description("C")]
+        Cruiser,
+
+        [Description("S")]
+        Submarine,
+
+        [Description("X")]
+        Hit,
+
+        [Description("M")]
+        Miss
+    }
+}
---
+++ battleship/BattleShip/BattleShip/Ships/ShipBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleShip.Board;
+
+namespace Ships
+{
+    public abstract class Ship{
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Hits { get; set; }
+        public OccupationType OccupationType { get; set; }
+        // retorna TRUE caso o numero de acertos forem maior ou igual ao seu tamanho
+        public bool shipIsDead
+        {
+            get{
+                return Hits >= Width;
+            }
+        }
+    }
+}
