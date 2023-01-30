c39906813f1712c8902d9e765747b1e3447d8794
Ewerthon-izi <ewer.izi@gmail.com>
2023-01-30 02:03:18 -0300

    add firing board

--- battleship/BattleShip/BattleShip/Program.cs
+++ battleship/BattleShip/BattleShip/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static void Main(string[] args)
+{
+    int player1Wins = 0, player2Wins = 0;
+
+    var numGames = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Player 1 Wins: " + player1Wins.ToString());
+    Console.WriteLine("Player 2 Wins: " + player2Wins.ToString());
+    Console.ReadLine();
+
+}
