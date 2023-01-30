7999ad662d3cb9de9db516ba2d5a0f4c15f8a326
Ewerthon-izi <ewer.izi@gmail.com>
2023-01-30 02:29:23 -0300

    add final main in progran

--- battleship/BattleShip/BattleShip/Program.cs
+++ battleship/BattleShip/BattleShip/Program.cs
@@ -1,12 +1,36 @@
 ﻿
-static void Main(string[] args)
+using BattleShip.Games;
+
+
+namespace BattleShip
 {
-    int player1Wins = 0, player2Wins = 0;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1Wins = 0, player2Wins = 0;
 
-    var numGames = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas jogos deseja jogar?");
+            var numGames = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Player 1 Wins: " + player1Wins.ToString());
-    Console.WriteLine("Player 2 Wins: " + player2Wins.ToString());
-    Console.ReadLine();
+            for (int i = 0; i < numGames; i++)
+            {
+                Game game1 = new Game();
+                game1.PlayToEnd();
+                if (game1.Player1.HasLost)
+                {
+                    player2Wins++;
+                }
+                else
+                {
+                    player1Wins++;
+                }
+            }
 
+            Console.WriteLine("Player 1 Wins: " + player1Wins.ToString());
+            Console.WriteLine("Player 2 Wins: " + player2Wins.ToString());
+            Console.ReadLine();
+
+        }
+    }
 }
